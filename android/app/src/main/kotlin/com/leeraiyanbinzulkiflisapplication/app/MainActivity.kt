b593f89ab4abbc8bffd227bdeb3833fde4b082ad
leeraiyan <woblek@gmail.com>
2023-11-15 15:08:56 +0100

    try

--- android/app/src/main/kotlin/com/leeraiyanbinzulkiflisapplication/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.leeraiyanbinzulkiflisapplication.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/apiClient/api_client.dart
+++
@@ -1,3 +0,0 @@
-import 'package:lee_raiyan_bin_zulkifli_s_application7/core/app_export.dart';
-
-class ApiClient extends GetConnect {}
